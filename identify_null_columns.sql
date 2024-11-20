99b9b594f26985ff75a28ad75fb077d39713f257
Walter Branson <wbranson@users.noreply.github.com>
2024-11-20 10:27:50 -0700

    Update identify_null_columns.sql

--- identify_null_columns.sql
+++ identify_null_columns.sql
@@ -3,18 +3,19 @@ DECLARE @TableName NVARCHAR(MAX)
 DECLARE @ColumnName NVARCHAR(MAX)
 DECLARE @SQL NVARCHAR(MAX)
 
--- Temporary table to store the results
+-- Temporary table to store all columns and their null-check status
 CREATE TABLE #NullColumns (
     SchemaName NVARCHAR(MAX),
     TableName NVARCHAR(MAX),
-    ColumnName NVARCHAR(MAX)
+    ColumnName NVARCHAR(MAX),
+    IsNull BIT DEFAULT 0 -- Default to 0, updated to 1 if all values are NULL
 )
 
--- Cursor to iterate through all tables and columns in the specified schema
-DECLARE column_cursor CURSOR FOR
+-- Populate the #NullColumns table with all columns from the specified schema
+INSERT INTO #NullColumns (SchemaName, TableName, ColumnName)
 SELECT 
     c.TABLE_SCHEMA,
-    t.TABLE_NAME,
+    c.TABLE_NAME,
     c.COLUMN_NAME
 FROM 
     INFORMATION_SCHEMA.COLUMNS c
@@ -24,6 +25,10 @@ WHERE
     t.TABLE_TYPE = 'BASE TABLE'
     AND c.TABLE_SCHEMA = @SchemaName -- Filter by schema name
 
+-- Cursor to iterate through each column in the #NullColumns table
+DECLARE column_cursor CURSOR FOR
+SELECT SchemaName, TableName, ColumnName FROM #NullColumns
+
 OPEN column_cursor
 FETCH NEXT FROM column_cursor INTO @SchemaName, @TableName, @ColumnName
 
@@ -32,9 +37,10 @@ BEGIN
     -- Build dynamic SQL to check if a column is NULL for all records
     SET @SQL = 'IF NOT EXISTS (SELECT 1 FROM ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName) +
                ' WHERE ' + QUOTENAME(@ColumnName) + ' IS NOT NULL) ' +
-               'INSERT INTO #NullColumns (SchemaName, TableName, ColumnName) VALUES (''' +
-               @SchemaName + ''', ''' + @TableName + ''', ''' + @ColumnName + ''')'
-               
+               'UPDATE #NullColumns ' +
+               'SET IsNull = 1 ' +
+               'WHERE SchemaName = ''' + @SchemaName + ''' AND TableName = ''' + @TableName + ''' AND ColumnName = ''' + @ColumnName + ''''
+    
     -- Execute the dynamic SQL
     EXEC sp_executesql @SQL
     
@@ -45,7 +51,7 @@ CLOSE column_cursor
 DEALLOCATE column_cursor
 
 -- Display results
-SELECT * FROM #NullColumns
+SELECT * FROM #NullColumns ORDER BY SchemaName, TableName, ColumnName
 
 -- Cleanup
 DROP TABLE #NullColumns
